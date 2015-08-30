ecbc5a73250d211a7230fbf404ba8f6229628247
Vincent Chau <vichau@ucdavis.edu>
2015-08-30 04:19:57 -0700

    added transition

---
+++ MatchUp/TransitionAnimator.h
@@ -0,0 +1,13 @@
+//
+//  TransitionAnimator.h
+//  MatchUp
+//
+//  Created by Vincent Chau on 8/30/15.
+//  Copyright (c) 2015 Vincent Chau. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TransitionAnimator : NSObject
+
+@end
---
+++ MatchUp/TransitionAnimator.m
@@ -0,0 +1,13 @@
+//
+//  TransitionAnimator.m
+//  MatchUp
+//
+//  Created by Vincent Chau on 8/30/15.
+//  Copyright (c) 2015 Vincent Chau. All rights reserved.
+//
+
+#import "TransitionAnimator.h"
+
+@implementation TransitionAnimator
+
+@end
